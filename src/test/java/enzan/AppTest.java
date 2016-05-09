549c3f1e44e8d71047192698865c997e400d246d
Tamaki Tatsunori <github@qtamaki.com>
2016-05-09 19:03:00 +0900

    add test

--- src/test/java/enzan/AppTest.java
+++ src/test/java/enzan/AppTest.java
@@ -1,38 +1,28 @@
 package enzan;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.PrintStream;
+import org.junit.Test;
+import static org.mockito.Matchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
+	@Test
     public void testApp()
     {
-        assertTrue( true );
+        PrintStream out = mock(PrintStream.class);
+        System.setOut(out);
+        App.main(new String[]{});
+        verify(out).println("hello");
+        verify(out).println("こんにちは");
+        verify(out).println(125);
     }
 }
